434d9f2bb2b0c8b34db87c8d6e0b5416cb699ecf
Xmausix <barbara.szymanska469@gmail.com>
2024-09-15 20:25:27 +0200

    Main Api update

---
+++ Krzyżówkomat/js/main.js
@@ -0,0 +1,17 @@
+function formatPhone(input) {
+    // Usuń wszystkie znaki, które nie są cyframi
+    let value = input.value.replace(/\D/g, '');
+
+    // Formatuj tekst z myślnikami po trzech cyfrach
+    if (value.length > 3 && value.length <= 6) {
+        input.value = value.slice(0, 3) + '-' + value.slice(3);
+    } else if (value.length > 6) {
+        input.value = value.slice(0, 3) + '-' + value.slice(3, 6) + '-' + value.slice(6, 9);
+    } else {
+        input.value = value;
+    }
+}
+
+
+
+const #btn = document.getElementById('btn');
